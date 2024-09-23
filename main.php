3118cae6aff69d07ce7f94b16964a68197fcbea5
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-23 14:37:48 +0800

    second commit for main and style files

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Main</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="header-div">
+
+        </div>
+        <div class="contents-div">
+            <div class="content-navigation"></div>
+            <div class="content-contents"></div>
+        </div>
+        <div class="footer-div">
+
+        </div>
+    </body>
+
+</html>
